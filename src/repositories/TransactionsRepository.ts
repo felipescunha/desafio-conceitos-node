f89067c711d3923d8e260d18d2bd5b7697a8e4ff
felipescunha <fcunha.ti@gmail.com>
2021-03-19 20:13:21 -0300

    desafio-conceitos-node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,15 @@
 import Transaction from '../models/Transaction';
 
-interface Balance {
+interface BalanceTDO {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface RequestTDO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -15,14 +21,46 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
+  public getBalance(): BalanceTDO {
     // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: BalanceTDO, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
+  public create({ value, title, type }: RequestTDO): Transaction {
     // TODO
+
+    const transaction = new Transaction({ value, title, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
